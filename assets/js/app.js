efc0aaec7d55f0926fdbef1a6699183d46b34ea1
bruere_s <sebastien.bruere@epitech.eu>
2016-11-07 17:05:40 +0100

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-
+	adaptArrowDown();
 });
 
+$(window).resize(function() {
+	adaptArrowDown();
+});
+
+function	adaptArrowDown() {
+	var x = $('.container-box-actions').width();
+
+	$('#arrow').css({'left': (x / 2) - 24});
+}
 
 
 
